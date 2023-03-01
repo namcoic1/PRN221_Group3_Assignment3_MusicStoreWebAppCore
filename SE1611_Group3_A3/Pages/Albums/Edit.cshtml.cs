2af77765f54a8b235d09cde869a5c61b24c60522
thanh-bung-5501 <thanhbung5501@gmail.com>
2023-03-01 15:51:38 +0700

    new source code

---
+++ SE1611_Group3_A3/Pages/Albums/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SE1611_Group3_A3.Models;
+
+namespace SE1611_Group3_A3.Pages.Albums
+{
+    public class EditModel : PageModel
+    {
+        private readonly SE1611_Group3_A3.Models.MusicStoreContext _context;
+
+        public EditModel(SE1611_Group3_A3.Models.MusicStoreContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Album Album { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Albums == null)
+            {
+                return NotFound();
+            }
+
+            var album =  await _context.Albums.FirstOrDefaultAsync(m => m.AlbumId == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            Album = album;
+           ViewData["ArtistId"] = new SelectList(_context.Artists, "ArtistId", "ArtistId");
+           ViewData["GenreId"] = new SelectList(_context.Genres, "GenreId", "GenreId");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Album).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlbumExists(Album.AlbumId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool AlbumExists(int id)
+        {
+          return _context.Albums.Any(e => e.AlbumId == id);
+        }
+    }
+}
